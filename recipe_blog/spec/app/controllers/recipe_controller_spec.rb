c3443994544b71e0c8f8082caa54ebe0244c4d1a
Kerri Marusiak <kerri.maru@gmail.com>
2018-03-18 11:57:43 +0000

    added test for recipe/add and recipe/id

--- recipe_blog/spec/app/controllers/recipe_controller_spec.rb
+++ recipe_blog/spec/app/controllers/recipe_controller_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe "/recipe" do
  
   before do
     @recipes = []
-    @recipes << Recipe.create!(name: "Chilli")
-    @recipes << Recipe.create!(name: "Curry")
-    @recipes << Recipe.create!(name: "Tiramisu")
+    @recipes << Recipe.create!(name: "Chilli", method: "add sour cream and eat")
+    @recipes << Recipe.create!(name: "Curry", method: "stir and cook and eat")
+    @recipes << Recipe.create!(name: "Tiramisu", method: "fluff up and eat")
   end
 
   describe "GET /recipe" do
@@ -15,7 +15,21 @@ RSpec.describe "/recipe" do
       expect(last_response.body).to include @recipes.first.name
       expect(last_response.body).to include @recipes.last.name
     end
+  end
 
-    
+  describe "GET /recipe/add" do
+    it "should display form to add a recipe" do
+      get '/recipe/add'
+      expect(last_response.body).to include "Title:"
+      expect(last_response.body).to include "Method:"
+    end    
+  end
+
+  describe "GET /recipe/#{:recipe_id}" do
+    it "should display recipe name and method" do
+      get '/recipe/2'
+      expect(last_response.body).to include @recipes.second.name
+      expect(last_response.body).to include @recipes.second.method
+    end    
   end
 end
